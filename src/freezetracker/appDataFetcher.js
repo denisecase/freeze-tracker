7582d0e5e13eefb2f7961fcfc5e52666306b516c
Denise Case <denisecase@gmail.com>
2023-04-28 21:58:23 -0500

    updated js path to data files

--- src/freezetracker/appDataFetcher.js
+++ src/freezetracker/appDataFetcher.js
@@ -1,7 +1,7 @@
 function getProcessedFilePath(fname) {
   const basePath = window.location.href.includes("localhost")
     ? "/root"
-    : "https://denisecase.github.io/freeze-tracker";
+    : "https://raw.githubusercontent.com/denisecase/freeze-tracker/main";
   const processedFilePath = `${basePath}/data/2_processed/${fname}`;
   console.log(`Reading from file ${processedFilePath}`);
   return processedFilePath;
